6ef373b3e7ced28bf5c90de466de8bf918f8b27b
ding <yiding.han@imaygou.com>
2016-03-25 16:27:29 +0800

    mod use wget instead of curl to get a better output

--- jdq_npm.sh
+++ jdq_npm.sh
@@ -2,7 +2,7 @@
 
 {
 
-if  [ $(curl -q ipinfo.io/country | grep 'HK') ]; then
+if  [ $(wget -qO- ipinfo.io/country | grep 'HK') ]; then
     npm --registry=https://registry.npm.taobao.org --cache=$HOME/.npm/.cache/cnpm --disturl=https://npm.taobao.org/dist --userconfig=$HOME/.cnpmrc "$@"
 else
     npm "$@"
