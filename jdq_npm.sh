6fc9821a6d23b5936d50527f4eecff9bf63b115e
ding <yiding.han@imaygou.com>
2016-03-25 16:30:54 +0800

    mod: use slient flag in curl

--- jdq_npm.sh
+++ jdq_npm.sh
@@ -2,7 +2,7 @@
 
 {
 
-if  [ $(wget -qO- ipinfo.io/country | grep 'HK') ]; then
+if  [ $(curl -s ipinfo.io/country | grep 'HK') ]; then
     npm --registry=https://registry.npm.taobao.org --cache=$HOME/.npm/.cache/cnpm --disturl=https://npm.taobao.org/dist --userconfig=$HOME/.cnpmrc "$@"
 else
     npm "$@"
